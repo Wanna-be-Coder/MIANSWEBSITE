13941a865f0fc93626e3cf534ac0f22987ecfc70
RJRatul <rafsan.ratul.mians@gmail.com>
2021-10-09 01:15:33 +0600

    M-003 first page done

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,22 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div >
-     <h1> MIANS </h1>
-     <h3>Reflection of your desire</h3>
-     <p>Hello</p>
+    <div
+      style={{
+        backgroundImage: `url(${require("./background-image.svg")})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        height: "100vh",
+        position: "fixed",
+        width: "100%"
+      }}
+      className="logo"
+      >
+        <Image src="/logo.png" width={400} height={150} />
+      
+      <h1>Reflection of your desire</h1>
+      
+      {/* <p>Hello</p>   */}
     </div>
   )
 }
